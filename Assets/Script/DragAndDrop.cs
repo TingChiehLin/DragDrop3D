472e09815651cf6d46b34b9203f6d1a60e78cd8a
Lin Ting Chieh <lintingchieh@Lins-MacBook-Pro.local>
2020-04-14 16:15:52 +1000

    first commit

---
+++ Assets/Script/DragAndDrop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    //How do we set cube movement
+    private Vector3 mOffset;
+    //How far away from our camera we are
+    private float mZCoord;
+
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(transform.position).z;
+        mOffset = transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+}
